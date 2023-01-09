c1e71f4942f637ad9b60647c2682afdb14735f72
Hoang Le <lebahoang010296@gmail.com>
2023-01-08 18:22:51 -0800

    Create 429. N-ary Tree Level Order Traversal.py

---
+++ binary_tree/429. N-ary Tree Level Order Traversal.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root: return []
+
+        ans = []
+        queue = [root]
+        while queue:
+            next_level = []
+            curr_level_ans = []
+            for node in queue:
+                curr_level_ans.append(node.val)
+                for child in node.children:
+                    if child: next_level.append(child)
+            queue = next_level
+            ans.append(curr_level_ans)
+        return ans
