8a777761cce95078c4af372f84effc5a31288ebc
Hoang Le <lebahoang010296@gmail.com>
2023-02-09 19:31:12 -0800

    Create 231. Power of Two.py

---
+++ math/231. Power of Two.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/power-of-two/description/
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        # if n <= 0: return False
+        # while abs(n) > 1:
+        #     if n % 2 != 0: return False
+        #     n = n // 2
+        # return True
+        if n == 0: return False
+        return n & (n - 1) == 0
