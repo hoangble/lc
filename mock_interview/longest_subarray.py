96858cbf6664aed512ec47258fd274567ebc532a
Hoang Le <lebahoang010296@gmail.com>
2023-02-25 18:10:37 -0800

    Update longest_subarray.py

--- mock_interview/longest_subarray.py
+++ mock_interview/longest_subarray.py
@@ -39,39 +39,93 @@ k = 0
 print(longest_subarray(prices, k))
 
 # 2D plot of lands
-# prices = [[1, 5, 3, 1], [2, 4, 6, 1], [2, 5, 9, 1]], k = 12, ans = 3
+# prices = [[1, 5, 3, 1], [2, 4, 6, 1], [2, 5, 9, 1]], k = 12, ans = 2
 # [[1, 5, 3, 1], 
 # [2, 4, 6, 1], 
 # [2, 5, 9, 1]]
+# approach: 
+# 1. start from the top left corner, we will increase the size until the sum > k
+# 2. move to the next point being the next left corner of the square, if the sum is small -> increase the size
+# 3. if we can't increase the size and rotate thru all possible left corner -> return k
 
 from typing import List 
 def longest_subarray_2d(prices: List[List], k: int) -> int:
-    m, n = len(prices), len(prices[0])
-    prefix_sum = [[0 for _ in range(n)] for _ in range(m)]
-
-    for i in range(m):
-        for j in range(n):
-            prefix_sum[i][j] = prefix_sum[i][j] + prices[i][j]
+    con_sum_ = consecutive_sum(prices)
     
     left_corner_x = 0
     left_corner_y = 0
 
-    while left_corner_x < n and left_corner_y < m:
-
-
-
+    m, n = len(prices), len(prices[0])
     ans = 0
+    while left_corner_x < n and left_corner_y < m:
+        while valid(left_corner_x + ans, left_corner_y + ans, m, n) and prefix_sum(con_sum_, left_corner_x, left_corner_y, left_corner_x + ans, left_corner_y + ans) <= k:
+            print(left_corner_x, left_corner_y, ans, prefix_sum(con_sum_, left_corner_x, left_corner_y, left_corner_x + ans, left_corner_y + ans))
+            ans += 1
+        left_corner_y += 1
+        if left_corner_y == n:
+            left_corner_y = 0
+            left_corner_x += 1
     return ans
 
-   0  1. 2  3
-0 [1, 2, 3, 4]
-1 [1, 2, 3, 4]
-2 [1, 2, 3, 8]
+def consecutive_sum(prices: List[int]) -> List[int]:
+    m, n = len(prices), len(prices[0])
 
-sum from [0, 0] to [2, 3]
+    prefix_sum = [[0 for _ in range(n)] for _ in range(m)]
 
-sum[2, 3] = array[2, 3] + sum[1, 3] + sum[2, 2] - sum[1, 2]
+    for i in range(m):
+        prev_row = i - 1 if i - 1 >= 0 else 0
+        for j in range(n):
+            prev_col = j - 1 if j - 1 >= 0 else 0
+            if i == 0:
+                prefix_sum[i][j] = prices[i][j] + prefix_sum[0][prev_col] 
+            elif j == 0:
+                prefix_sum[i][j] = prices[i][j] + prefix_sum[prev_row][0] 
+            else:
+                prefix_sum[i][j] = prices[i][j] + prefix_sum[prev_row][j] + prefix_sum[i][prev_col] - prefix_sum[prev_row][prev_col]
+    return prefix_sum
 
-sum[i, j] = array[i, j] + sum[i-1, j] + sum[i, j-1] - sum[i-1, j-1]
+def prefix_sum(prefix_sum: List[int], x0: int, y0: int, x1: int, y1: int) -> List[int]:
+    prev_x0 = x0 - 1 if x0 - 1 >= 0 else 0
+    prev_y0 = y0 - 1 if y0 - 1 >= 0 else 0
+    if x0 == 0 or y0 == 0:
+        prefix_sum[prev_x0][prev_y0] = 0
+    if x0 == 0:
+        prefix_sum[prev_x0][y1] = 0
+    if y0 == 0:
+        prefix_sum[x1][prev_y0] = 0
+    
+    return prefix_sum[x1][y1] - prefix_sum[x1][prev_y0] - prefix_sum[prev_x0][y1] + prefix_sum[prev_x0][prev_y0]
 
-sum(a, b, x, y) = sum[x, y] - sum[x, b-1] - sum[a-1, y] + sum[a-1, b-1]
+
+def valid(x, y, m , n):
+    return 0 <= x < m and 0 <= y < n
+
+####
+# Example and explanation of prefix sum
+#    0  1. 2  3
+# 0 [1, 2, 3, 4]
+# 1 [1, 2, 3, 4]
+# 2 [1, 2, 3, 8]
+
+# sum from [0, 0] to [2, 3]
+
+# sum[2, 3] = array[2, 3] + sum[1, 3] + sum[2, 2] - sum[1, 2]
+
+# sum[i, j] = array[i, j] + sum[i-1, j] + sum[i, j-1] - sum[i-1, j-1]
+
+# sum(x0, y0, x, y) = sum[x, y] - sum[x, y0-1] - sum[x0-1, y] + sum[x0-1, y0-1]
+def print_mat(mat):
+    for row in mat:
+        print(row)
+
+
+if __name__ == "__main__":
+    prices = [[1, 5, 3, 1], [2, 4, 6, 1], [2, 5, 9, 1]]
+    print(longest_subarray_2d(prices, 37))
+    # print_mat(prices)
+    # print("prefix sum")
+    # con_sum_ = consecutive_sum(prices)
+    # print_mat(con_sum_)
+    # print(prefix_sum(con_sum_, x0= 0, y0 = 0, x1 = 1, y1 = 1))
+
+
