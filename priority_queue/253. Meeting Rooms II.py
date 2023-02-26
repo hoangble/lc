608afc98860e91a0d54041dc2154f8cbc4faf775
Hoang Le <lebahoang010296@gmail.com>
2023-02-25 21:12:26 -0800

    Create 253. Meeting Rooms II.py

---
+++ priority_queue/253. Meeting Rooms II.py
@@ -0,0 +1,16 @@
+class Solution:
+    import heapq
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        q = []
+        heapq.heapify(q)
+        intervals.sort(key = lambda x: x[0])
+        heapq.heappush(q, intervals[0][-1])
+        # print(q)
+        ans = 1
+        for i in intervals[1:]:
+            # print(i, q)
+            while len(q) > 0 and i[0] >= q[0]:
+                heapq.heappop(q)
+            heapq.heappush(q, i[-1])
+            ans = max(ans, len(q))
+        return ans
