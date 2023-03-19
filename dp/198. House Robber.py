86e16c17b439c7929a17e77fb7b5e824d7c1fdaf
Hoang Le <lebahoang010296@gmail.com>
2023-03-18 17:03:04 -0700

    Update 198. House Robber.py

--- dp/198. House Robber.py
+++ dp/198. House Robber.py
@@ -1,1 +1,13 @@
-pass
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        a = [None] * len(nums)
+        a[0] = nums[0]
+        a[1] = max(nums[1], a[0])
+
+        for i in range(2, len(nums)):
+            a[i] = max(a[i-2] + nums[i], a[i-1])
+
+        return a[-1]
