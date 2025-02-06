d260c338bb0db4bf03e82460d8764772b8100757
Hoang Le <lebahoang010296@gmail.com>
2025-02-05 22:09:51 -0800

    [8] [Time Beats: 100.00%] [Memory Beats: 7.24%] - LeetPush

--- 8-String-to-Integer-(atoi).py
+++ 8-String-to-Integer-(atoi).py
@@ -1,14 +1,20 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        ans = []
+        ans = 0
 
         number = False
         negative = False
-        for s_ in s:
+        MIN = -2 ** 31
+        MAX = 2 ** 31 - 1
+
+        for i, s_ in enumerate(s):
             if number: 
-                if not s_.isnumeric():
+                if ord('0') > ord(s_) or ord(s_) >  ord('9'):
                     break
-                ans.append(s_)
+                if ans > MAX // 10 or (ans == MAX // 10) and int(s_) > MAX % 10:
+                    return MAX if not negative else MIN
+                ans = ans * 10 + int(s_)
+
             else:
                 if s_ == \ \:
                     continue
@@ -21,21 +27,7 @@ class Solution:
                 elif ord('0') <= ord(s_) <= ord('9'):
                     number = True
                     if s_ != '0':
-                        ans.append(s_)
+                        ans = ans * 10 + int(s_)
                 else:
                     break
-        
-        ans_num = 0
-        for i in range(len(ans)):
-            ans_num += int(ans[i]) * 10 ** (len(ans) - i - 1)
-        
-        ans = ans_num if not negative else -ans_num
-        # rounding
-        if ans < 0:
-            MIN = -2 ** 31
-            ans = max(ans, MIN)
-        else:
-            MAX = 2 ** 31 - 1
-            ans = min(ans, MAX)
-        return ans
-            
+        return ans if not negative else -ans
