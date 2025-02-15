038f7a849e68438e2c888da7b4953f1555ba26da
Hoang Le <lebahoang010296@gmail.com>
2025-02-15 11:06:05 -0800

    [543] [Time Beats: 5.03%] [Memory Beats: 5.17%] - LeetPush

--- 543-Diameter-of-Binary-Tree.py
+++ 543-Diameter-of-Binary-Tree.py
@@ -7,14 +7,25 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.dia = 0
+        self.path = []
         self.dfs(root)
         return self.dia
     
-    def dfs(self, node) -> int:
+    def dfs(self, node) -> Tuple[int, List[TreeNode]]:
         if not node: 
-            return 0
+            return 0, [None]
 
-        left_dia = self.dfs(node.left)
-        right_dia = self.dfs(node.right)
-        self.dia = max(self.dia, left_dia + right_dia)
-        return max(left_dia, right_dia) + 1
+        left_dia, left_dia_path = self.dfs(node.left)
+        right_dia, right_dia_path = self.dfs(node.right)
+
+        if left_dia + right_dia > self.dia:
+            self.dia = left_dia + right_dia
+            self.path = left_dia_path + [node] +right_dia_path
+        # self.dia = max(self.dia, left_dia + right_dia)
+
+        if left_dia > right_dia:
+            return left_dia + 1, left_dia_path + [node]
+        else:
+            return right_dia + 1, right_dia_path + [node]
+
+        # return max(left_dia, right_dia) + 1
