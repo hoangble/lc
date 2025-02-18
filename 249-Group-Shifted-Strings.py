f61efb8c078ff2e6a0e6d6eae5339365268fc96c
Hoang Le <lebahoang010296@gmail.com>
2025-02-18 08:42:55 -0800

    [249] [Time Beats: 100.00%] [Memory Beats: 67.95%] - LeetPush

--- 249-Group-Shifted-Strings.py
+++ 249-Group-Shifted-Strings.py
@@ -1,16 +1,14 @@
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
-        # convert everthing to how an a-starting string
         group = defaultdict(list)
         for s in strings:
             hash_key = self.get_hash(s)
             group[hash_key].append(s)
-        
         return list(group.values())
-
+    
     def get_hash(self, s):
-        key = []
+        curr = []
         for a, b in zip(s, s[1:]):
-            curr = (ord(a)-ord(b)) % 26 + ord('a')
-            key.append(chr(curr))
-        return ''.join(key)
+            k = (ord(a) - ord(b)) % 26
+            curr.append(k)
+        return tuple(curr)
