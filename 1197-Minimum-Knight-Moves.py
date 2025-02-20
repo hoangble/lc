02f7485b37f32b19e598e4f84d0ce840f668db2c
Hoang Le <lebahoang010296@gmail.com>
2025-02-19 22:30:26 -0800

    [1197] [Time Beats: 89.06%] [Memory Beats: 97.70%] - LeetPush

--- 1197-Minimum-Knight-Moves.py
+++ 1197-Minimum-Knight-Moves.py
@@ -2,6 +2,17 @@ class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         memo = {}
         return self.dfs(abs(x), abs(y), memo)
+
+        # O(1)
+        # x, y = abs(x), abs(y)
+        # if (x < y): x, y = y, x
+        # if (x == 1 and y == 0): return 3        
+        # if (x == 2 and y == 2): return 4        
+        # delta = x - y
+        # if (y > delta):
+        #     return delta - 2 * ((delta - y) // 3)
+        # else:
+        #     return delta - 2 * ((delta - y) // 4)
         
     def dfs(self, x, y, memo):
         if (x, y) in memo:
