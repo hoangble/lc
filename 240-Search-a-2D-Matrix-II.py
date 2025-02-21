4191f7c3e6187a28e6650d24f8d29ee9d47c268c
Hoang Le <lebahoang010296@gmail.com>
2025-02-21 09:05:47 -0800

    [240] [Time Beats: 93.57%] [Memory Beats: 51.07%] - LeetPush

---
+++ 240-Search-a-2D-Matrix-II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+
+        row = m - 1
+        col = 0
+        while row >= 0 and col < n:
+            if matrix[row][col] > target:
+                row -= 1
+            elif matrix[row][col] < target:
+                col += 1
+            else:
+                return True
+        return False
