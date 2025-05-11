9e75176cfa5627af270ac1a1e4d12461af636eb3
hoang <lebahoang010296@gmail.com>
2025-05-10 21:21:56 -0700

    starting google prep

---
+++ google-prep/watch-tower.py
@@ -0,0 +1,14 @@
+# Q1: A town is building a watchtower. The watchtower is located at (0, 0). Each unit height of the watchtower has a cost H. 
+# There are N houses located at (x, y) coordinates. Each house will pay cost C if it comes under the surveillance of the watchtower. 
+# The horizontal distance covered by the watchtower is the same as it's height. Find out the max profit you can make.
+
+# Inputs:
+
+# N number of houses
+# list of (x, y) coordinates
+# H cost to build unit height
+# C cost each house pays the watchtower
+# In the beginning the watchtower was located at the origin. 
+# 
+# ##########################################################################################################################
+# For the followup he said watchtower location will be provided. The (x, y) coordinates can be floats as well as H and C.
